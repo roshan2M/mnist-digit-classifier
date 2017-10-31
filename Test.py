ba8b85120f4500423a5002670b88d6525ef692d1
Roshan Munjal <roshan.munjal@hotmail.com>
2017-10-31 00:15:37 -0400

    Modifications

--- Test.py
+++ Test.py
@@ -8,23 +8,9 @@ import matplotlib.pyplot as plt
 from keras.utils import np_utils
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-# plot 4 images as gray scale
-plt.subplot(221)
-plt.imshow(X_train[0], cmap=plt.get_cmap('gray'))
-plt.subplot(222)
-plt.imshow(X_train[1], cmap=plt.get_cmap('gray'))
-plt.subplot(223)
-plt.imshow(X_train[2], cmap=plt.get_cmap('gray'))
-plt.subplot(224)
-plt.imshow(X_train[3], cmap=plt.get_cmap('gray'))
-# show the plot
-plt.show()
-
-# fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
 
-# load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 # flatten 28*28 images to a 784 vector for each image
@@ -32,7 +18,7 @@ num_pixels = X_train.shape[1] * X_train.shape[2]
 X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32')
 
-# normalize inputs from 0-255 to 0-1
+# normalize inputs
 X_train = X_train / 255
 X_test = X_test / 255
 
@@ -45,21 +31,19 @@ print X_test.shape
 
 # define baseline model
 def baseline_model():
-    # create model
     model = Sequential()
     model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
     model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
-    # Compile model
+	
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-
-# build the model
 model = baseline_model()
-# Fit the model
+# Applying the model to the training set
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
-# Final evaluation of the model
+# Evaluating epochs and overall accuracy of model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Baseline Error: %.2f%%" % (100-scores[1]*100))
 
+# Saving the model for future use
 model.save('my_mnist_keras_model.h5')
